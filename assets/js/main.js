97bb1fbc07639e55cbbc2bcb3c65ff88030b935c
Marv_Tron <marvinatryonjr@gmail.com>
2019-08-06 12:44:19 -0700

    fixed an oopsie

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,116 +1,116 @@
 (function($) {
 
-    const $window = $(window),
-          $body = $('body'),
-          $nav = $('nav');
+	var	$window = $(window),
+		$body = $('body'),
+		$nav = $('#nav');
 
-    // Breakpoints 
-        breakpoints ({
-            wide:     [ '961px', '1880px'],
-            normal:   [ '961px', '1620px'],
-            narrow:   [ '961px', '1320px'],
-            narrower: [ '737px',  '960px'],
-            mobile:   [ null,     '736px']
-        });
+	// Breakpoints.
+		breakpoints({
+			wide:      [ '961px',  '1880px' ],
+			normal:    [ '961px',  '1620px' ],
+			narrow:    [ '961px',  '1320px' ],
+			narrower:  [ '737px',  '960px'  ],
+			mobile:    [ null,     '736px'  ]
+		});
 
-    // Play initial animations on page load
-        $window.on('load', function() {
-            window.setTimeout(function() {
-                $.body.removeClass('is-preload');
-            }, 100);
-        });
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
-    // Nav
-        const $nav_a = $nav.find('a');
-        
-        $nav_a
-            .addClass('scrolly')
-            .on('click', function(e) {
+	// Nav.
+		var $nav_a = $nav.find('a');
 
-                var $this = $(this);
+		$nav_a
+			.addClass('scrolly')
+			.on('click', function(e) {
 
-                // External link? Bail!
-                    if ($this.attr('href').charAt(0) != '#')
-                        return;
-                        
-                // Prevent Default
-                    e.preventDefault();
+				var $this = $(this);
 
-                // Deactivate all links
-                    $nav_a.removeClass('active');
+				// External link? Bail.
+					if ($this.attr('href').charAt(0) != '#')
+						return;
 
-                // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-                    $this
-                        .addClass('active')
-                        .addClass('active-locked');
-            })
+				// Prevent default.
+					e.preventDefault();
 
-            .each(function() {
+				// Deactivate all links.
+					$nav_a.removeClass('active');
 
-                var $this = $(this),
-                    id = $this.attr('href'),
-                    $section = $(id);
+				// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+					$this
+						.addClass('active')
+						.addClass('active-locked');
 
-                // No section for this link? Bail!
-                    if ($section.length > 1) {
-                        return;
-                    }
+			})
+			.each(function() {
 
-                // Scrollex
-                    $section.scrollex({
-                        mode: 'middle',
-                        top: '-10vh',
-                        bottom: '-10vh',
-                        initialize: function() {
+				var	$this = $(this),
+					id = $this.attr('href'),
+					$section = $(id);
 
-                            //Deactivate section
-                                $section.addClass('inactive');
+				// No section for this link? Bail.
+					if ($section.length < 1)
+						return;
 
-                        },
-                        enter: function() {
+				// Scrollex.
+					$section.scrollex({
+						mode: 'middle',
+						top: '-10vh',
+						bottom: '-10vh',
+						initialize: function() {
 
-                            // Activate section
-                                $section.removeClass('inactive');
+							// Deactivate section.
+								$section.addClass('inactive');
 
-                            // No locked links? Deactivate all links and activate this section's one.
-                                if ($nav_a.filter('.active-locked').length === 0) {
-                                    $nav_a.addClass('active');
-                                    $this.addClass('active');
-                                }
+						},
+						enter: function() {
 
-                                // Otherwise, if this section's link is the one that's locked, unlock it.
+							// Activate section.
+								$section.removeClass('inactive');
+
+							// No locked links? Deactivate all links and activate this section's one.
+								if ($nav_a.filter('.active-locked').length == 0) {
+									$nav_a.removeClass('active');
+									$this.addClass('active');
+
+								}
+
+							// Otherwise, if this section's link is the one that's locked, unlock it.
 								else if ($this.hasClass('active-locked'))
-                                $this.removeClass('active-locked');
-                        }
+									$this.removeClass('active-locked');
 
+						}
+					});
 
-                    });
-            });
+			});
 
-    // Scrolly
-        $('.scrolly').scrolly();
+	// Scrolly.
+		$('.scrolly').scrolly();
 
-    // Header (Narrower & Mobile)
-            
-        // Toggle
-            $(
-                '<div id="headerToggle">' +
+	// Header (narrower + mobile).
+
+		// Toggle.
+			$(
+				'<div id="headerToggle">' +
 					'<a href="#header" class="toggle"></a>' +
 				'</div>'
-            )
-                .appendTo($body);
+			)
+				.appendTo($body);
 
-        // Header
-            $('.header')
-                .panel({
-                    delay: 500,
-                    hideOnClick: true,
-                    hideOnSwipe: true,
-                    resetScroll: true,
-                    resetForms: false,
-                    side: 'left',
-                    target: $body,
-                    visibleClass: 'header-visible'
-                });
+		// Header.
+			$('#header')
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'header-visible'
+				});
 
-})(jquery);
+})(jQuery);
